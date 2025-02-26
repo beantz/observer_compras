e06a398ac224e9a1d8a63586545a94836e4f6605
beantz123 <trizmours2002@gmail.com>
2025-02-26 15:22:34 -0300

    Removidos arquivos desnecessários

--- Documentos/code-laravel/compras-app/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\PedidoController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/pedido/{pedido_id}', [PedidoController::class, 'concluir']);
