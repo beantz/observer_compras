a72410f9b8d6b440f5fc1e1d9e7b7662625bb6f0
Beatriz Moura <trizmours2002@gmail.com>
2025-03-13 23:27:57 -0300

    Delete Documentos/js directory

--- Documentos/js/api_node/src/route/login.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import loginController from '../controllers/login.js';
-
-const route = new Router();
-
-route.post('/login', (req, res) => loginController.auth(req, res));
-
-export default route;
